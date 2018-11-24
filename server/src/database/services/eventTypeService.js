dae516d0e0ec6b9e552aeb22eec22dbfe8ed1f4e
Pascal Ledru <pascal.ledru@oracle.com>
2018-11-24 14:21:32 -0700

    remove events table

--- server/src/database/services/eventTypeService.js
+++ server/src/database/services/eventTypeService.js
@@ -1,72 +1,19 @@
 const AWS = require('aws-sdk')
 const db = require('./db')
+const BaseService = require('./baseService')
 
 db.init
 
-const dynamodb = new AWS.DynamoDB()
 const docClient = new AWS.DynamoDB.DocumentClient()
 
-class EventTypeService {
+class EventTypeService extends BaseService {
 
-  constructor() {
-    this.tableName = db.name + '_' + db.mode + '_' + 'EventTypes'
+  getName() {
+    return 'EventTypes'
   }
 
-  createTable() {
-    let params = {
-      TableName: this.tableName,
-      KeySchema: [
-        { AttributeName: "id", KeyType: "HASH"},
-        { AttributeName: "description", KeyType: "RANGE"}
-      ],
-      AttributeDefinitions: [
-        { AttributeName: "id", AttributeType: "N" },
-        { AttributeName: "description", AttributeType: "S" }
-      ],
-      ProvisionedThroughput: {
-        ReadCapacityUnits: 10,
-        WriteCapacityUnits: 10
-      }
-    }
-    return new Promise((resolve, reject) => {
-      dynamodb.createTable(params, (err, data) => {
-        if (err) {
-          reject(err)
-        } else {
-          resolve(data)
-        }
-      })
-    })
-  }
-
-  deleteTable() {
-    let params = {
-      TableName: this.tableName
-    }
-    return new Promise((resolve, reject) => {
-      dynamodb.deleteTable(params, (err, data) => {
-        if (err) {
-          reject(err)
-        } else {
-          resolve(data)
-        }
-      })
-    })
-  }
-
-  list() {
-    let params = {
-      TableName: this.tableName
-    }
-    return new Promise((resolve, reject) => {
-      docClient.scan(params, (err, data) => {
-        if (err) {
-          reject(err)
-        } else {
-          resolve(data)
-        }
-      })
-    })
+  getKey() {
+    return 'id'
   }
 
   add(eventType) {
