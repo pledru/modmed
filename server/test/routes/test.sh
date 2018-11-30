312b4a9ed2499878a2fb9f2d59f1caf718eba395
Pascal Ledru <pascal.ledru@oracle.com>
2018-11-29 18:14:55 -0700

    event

--- server/test/routes/test.sh
+++ server/test/routes/test.sh
@@ -11,3 +11,10 @@ curl localhost:8081/login -XPOST -i -H "Content-Type: application/json" -d '{"em
 
 curl localhost:8081/eventtypes -XGET
 
+curl localhost:8081/events -XGET -H "x-access-token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RAeHl6LmNvbSIsImlkIjowLCJpYXQiOjE1NDMwOTY1NzQsImV4cCI6MTU0MzE4Mjk3NH0.MveWNTMgDGVzaGXY-4whmgQYuJLgcu3plIZoMpLkM1s"
+
+curl localhost:8081 -XPOST -H "x-access-token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RAeHl6LmNvbSIsImlkIjowLCJpYXQiOjE1NDMwOTY1NzQsImV4cCI6MTU0MzE4Mjk3NH0.MveWNTMgDGVzaGXY-4whmgQYuJLgcu3plIZoMpLkM1s" -d {"types": [1,2,3]}
+
+curl localhost:8081/events -XPOST -H "Content-Type: application/json" -H "x-access-token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RAeHl6LmNvbSIsImlkIjowLCJpYXQiOjE1NDMwOTY1NzQsImV4cCI6MTU0MzE4Mjk3NH0.MveWNTMgDGVzaGXY-4whmgQYuJLgcu3plIZoMpLkM1s" -d '{"types": [1,2,3]}'
+
+curl localhost:8081/events -v -XPOST -H "Content-Type: application/json" -H "x-access-token:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RAeHl6LmNvbSIsImlkIjowLCJpYXQiOjE1NDMwOTg1NzksImV4cCI6MTU0MzE4NDk3OX0.-Au5WZrdfJRRPnm5CwGU9FWlW0DzhUZ5KSn0BbxgwdY" -d '{"types": [1,2,3]}'
