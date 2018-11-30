cc600d70be9b201f6e6d410cd9dce57083dd7b42
Pascal Ledru <pascal.ledru@oracle.com>
2018-11-29 18:16:43 -0700

    typos

--- server/src/config.js
+++ server/src/config.js
@@ -1,6 +1,6 @@
 const environment = process.env.NODE_ENV || 'test'
-const allConfig = require('./config.json');
-const config = allConfig[environment];
+const allConfig = require('./config.json')
+const config = allConfig[environment]
 
 module.exports = {
   config
